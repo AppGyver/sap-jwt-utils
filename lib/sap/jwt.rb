293dd170a9fce27b4ee87d9ba93440a1ac5ecc9a
Petrus Repo <petrus.repo@iki.fi>
2021-07-28 17:51:23 +0300

    Fetch JWKs

--- lib/sap/jwt.rb
+++ lib/sap/jwt.rb
@@ -7,13 +7,35 @@ require "multi_json"
 module Sap
   module Jwt
     class FetchJwksError < StandardError; end
+
     class FetchOpenIdConfigurationError < StandardError; end
+
     class MissingAccessTokenError < StandardError; end
 
     def self.request_headers
       {
         "User-Agent" => "appgyver/sap-jwt"
       }
+    end
+
+    # Fetch one or multiple JWKs which are used for verifying the token signature.
+    #
+    # The JWK URL should be retrieved from the discovery endpoint.
+    # The "same" JWK URL is also present in the JWT Token's Header section, but according to the
+    # OIDC specification, "ID tokens SHOULD NOT use the `jku` or `jwk` header parameter fields."
+    #
+    # In multi tenancy scenarios, the JWKs must be downloaded from an SAP-owned domain
+    # and not customer-controlled domains.
+    #
+    # https://github.wdf.sap.corp/pages/CPSecurity/Knowledge-Base/03_ApplicationSecurity/TokenValidation/
+    def self.fetch_jwks(site)
+      jwks_uri = fetch_openid_configuration(site)[:jwks_uri]
+
+      response = Faraday.get(jwks_uri, request_headers)
+
+      raise FetchJwksError, "Failed to fetch #{jwks_uri}" unless response.success?
+
+      MultiJson.load(response.body, symbolize_names: true)
     end
 
     # Authentication endpoint info (tenant specific)
--- spec/sap/jwt_spec.rb
+++ spec/sap/jwt_spec.rb
@@ -1,15 +1,19 @@
 # frozen_string_literal: true
 
 RSpec.describe Sap::Jwt do
-  it "has a version number" do
-    expect(Sap::Jwt::VERSION).not_to be nil
-  end
+  let(:site) { "https://subdomain-something.example.com" }
+  let(:path) { "/.well-known/test-openid-configuration" }
+  let(:url) { [site, path].join }
+
   let(:default_headers) do
     {
       "User-Agent" => "appgyver/sap-jwt"
     }
   end
 
+  it "has a version number" do
+    expect(Sap::Jwt::VERSION).not_to be nil
+  end
 
   it "decodes token with options"
 
@@ -28,12 +32,58 @@ RSpec.describe Sap::Jwt do
     end
   end
 
+  describe ".fetch_jwks" do
+    let(:jwks_uri) { "https://sap-provisioning.authentication.sap.example.com/token_keys" }
+
+    let(:response) { instance_double(Faraday::Response) }
+
+    let(:body_string) { '{"it": "works, jwk here"}' }
+    let(:body_parsed) { { it: "works, jwk here" } }
+
+    before do
+      allow(Faraday)
+        .to receive(:get)
+        .with(jwks_uri, default_headers)
+        .and_return response
+
+      allow(response)
+        .to receive(:body)
+        .and_return body_string
+
+      allow(described_class)
+        .to receive(:fetch_openid_configuration)
+        .and_return({ jwks_uri: jwks_uri })
+    end
+
+    context "when success" do
+      before do
+        allow(response)
+          .to receive(:success?)
+          .and_return true
+      end
+
+      it "returns json" do
+        expect(described_class.fetch_jwks(site))
+          .to eq(body_parsed)
+      end
+    end
+
+    context "when failure" do
+      before do
+        allow(response)
+          .to receive(:success?)
+          .and_return false
+      end
+
+      it 'raises FetchJwksError' do
+        expect do
+          described_class.fetch_jwks(site)
+        end.to raise_error Sap::Jwt::FetchJwksError
+      end
+    end
+  end
+
   describe ".fetch_openid_configuration" do
-    let(:site) { "https://subdomain-something.example.com" }
-    let(:path) { "/.well-known/test-openid-configuration" }
-
-    let(:url) { [site, path].join }
-
     let(:response) { instance_double(Faraday::Response) }
     let(:body_string) { '{"it": "works"}' }
     let(:body_parsed) { { it: "works" } }
